d8f58ffa2937f23ff9197ac92c04bf53fad0fec1
Johan Setiawan <jokerzetia077@gmail.com>
2024-02-21 09:31:17 +0700

    Update rabbitmq.go

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -63,7 +63,7 @@ func Dial(url string, tlsParam *tls.Config) (*Connection, error) {
 	var conn Connection
 	var err error
 	
-	if tlsParam == new(tls.Config) {
+	if tlsParam == nil) {
 		conn, err = amqp.Dial(url)
 		if err != nil {
 			return nil, err
